c266f34b2fb5fdfdb1af0333f5d8a6401ff3ffd0
Diptamoy Mitra <91617575+Diptamoy-Mitra@users.noreply.github.com>
2022-04-26 02:35:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const quoteText = document.querySelector(".quote");
+const quoteBtn = document.querySelector("button");
+const authorName = document.querySelector(".author  .name")
+const soundBtn = document.querySelector(".sound");
+const copyBtn = document.querySelector(".copy");
+const twitterBtn = document.querySelector(".twitter");
+
+function
+randomQuote() {
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loding Quote.....";
+    //fetching random quotes from api  and parsing it into js object
+    fetch("https://api.quotable.io/random").then(res => res.json()).then(result => {
+        console.log(result);
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+    });
+}
+soundBtn.addEventListener("click", () => {
+    //the  SpeechSynthesisUtterance  is a web speech api that represents a speech request
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${authorName.innerText}`);
+    speechSynthesis.speak(utterance);
+});
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+twitterBtn.addEventListener("click", () => {
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+})
+quoteBtn.addEventListener("click", randomQuote);
